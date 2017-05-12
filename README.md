e0d70ea30b0565d90d339db006d77080dc5077ba
Theron Burger <theron@theronburger.com>
2017-05-12 17:09:54 +0200

    changes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cqCoder
 QR code MQTT fun!
+
+# Setup
+* [Install nodeJS current](https://nodejs.org/en/)
+* In terminal, run npm install
+
+# Usage
+* Open up app.js in a text editor
+* Semi self explanitory, modify the for loop and file names etc
+* in terminal, run node app.js
--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var qr = require('qr-image')
-
-
+var mkdirp = require('mkdirp');
+    
 function pad(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
     return s;
 }
 
+mkdirp('images', function (err) {
+    if (err) console.error(err)
+});
+
+
 for (i = 0; i < 200; i++){
 	var id = pad(i,10);
 	var qr_svg = qr.image(id,{type : "svg"});
-	qr_svg.pipe(require('fs').createWriteStream(id+'.svg'));
+	qr_svg.pipe(require('fs').createWriteStream("images/"+id+'.svg'));
 }
